cb05f6baa3316c755301fed3ac7e07dba39027da
Alexandrine Dube <adube@c1r1p4.42quebec.com>
2023-10-27 15:21:10 -0400

    Makefile

--- philo/src/actions.c
+++ philo/src/actions.c
@@ -1,29 +1,13 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   actionc.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: adube <adube@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/10/27 11:30:32 by adube             #+#    #+#             */
-/*   Updated: 2023/10/27 15:17:03 by adube            ###   ########.fr       */
+/*   Created: 2023/10/27 15:06:53 by adube             #+#    #+#             */
+/*   Updated: 2023/10/27 15:17:32 by adube            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/philo.h"
-
-void	*routine(void)
-{
-	printf("Test from threads");
-}
-
-int	main(int argc, char **argv)
-{
-	pthread_t	t1;
-
-	if (pthread_create(&t1, NULL, &routine, NULL) != 0)
-		return (1);
-	if (pthread_join(t1, NULL) != 0)
-		return (1);
-	return (0);
-}
--- philo/src/utils.c
+++ philo/src/utils.c
@@ -1,29 +1,13 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   utils.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: adube <adube@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/10/27 11:30:32 by adube             #+#    #+#             */
-/*   Updated: 2023/10/27 15:17:03 by adube            ###   ########.fr       */
+/*   Created: 2023/10/27 15:06:59 by adube             #+#    #+#             */
+/*   Updated: 2023/10/27 15:17:25 by adube            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/philo.h"
-
-void	*routine(void)
-{
-	printf("Test from threads");
-}
-
-int	main(int argc, char **argv)
-{
-	pthread_t	t1;
-
-	if (pthread_create(&t1, NULL, &routine, NULL) != 0)
-		return (1);
-	if (pthread_join(t1, NULL) != 0)
-		return (1);
-	return (0);
-}
