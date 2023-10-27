223e38b4493d8a692c87f6498fc94d326dd56484
Alexandrine Dube <adube@c1r1p4.42quebec.com>
2023-10-27 15:05:52 -0400

    test threads

--- philo/src/main.c
+++ philo/src/main.c
@@ -6,7 +6,23 @@
 /*   By: adube <adube@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/27 11:30:32 by adube             #+#    #+#             */
-/*   Updated: 2023/10/27 11:30:33 by adube            ###   ########.fr       */
+/*   Updated: 2023/10/27 14:40:00 by adube            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "../include/philo.h"
+
+void	*routine()
+{
+	printf("Test from threads");
+}
+
+int	main(int argc, char **argv)
+{
+	pthread_t t1;
+	if (pthread_create(&t1, NULL, &routine, NULL) != 0)
+		return (1);
+	if (pthread_join(t1, NULL) != 0)
+		return (1);
+	return (0);
+}
